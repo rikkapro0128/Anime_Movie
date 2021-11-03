9402ccc6b9684741dae112b9b46176c343b1bd58
rikkapro0128 <stkssc002@gmail.com>
2021-11-03 11:55:57 +0700

    upload code by miru => code part anime details

---
+++ frontend/src/components/Checkbox.vue
@@ -0,0 +1,82 @@
+<template>
+  <div
+    class="check-box"
+    :class="{ active: stateCheck }"
+    :position-checkbox="checkboxPos"
+    @click="(stateCheck = !stateCheck), changeCheckBox(stateCheck)"
+  >
+    <i class="fas fa-plus"></i>
+    <span class="check-box__name" :title="name">{{ name }}</span>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import anime from "animejs";
+
+export default {
+  props: {
+    checkboxPos: {
+      type: Number,
+    },
+    name: {
+      type: String,
+    },
+    value: {
+      type: Boolean,
+    },
+  },
+  setup(props) {
+    const stateCheck = ref(props.value);
+    return { stateCheck };
+  },
+  methods: {
+    changeCheckBox(value) {
+      // change spin
+      anime({
+        targets: `.check-box[position-checkbox="${this.$props.checkboxPos}"] i`,
+        rotate: value ? 45 : 0,
+      });
+      // emit toggle checkbox
+      this.$emit("ChangeCheckBox", { name: this.$props.name, value });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.check-box {
+  padding: 0.5rem;
+  background-color: $main-color;
+  border-radius: 10px;
+  border: 2px solid $border-checkbox;
+  cursor: pointer;
+  margin: 0.5rem;
+  display: flex;
+  align-items: center;
+  transition: box-shadow top left 0.2s ease-in-out;
+  box-shadow: 2px 2px 0px $main-color;
+  position: relative;
+  &.active {
+    box-shadow: 0px 0px 0px $main-color;
+    top: 1px;
+    left: 1px;
+    span,
+    i {
+      color: $active-checkbox;
+    }
+  }
+  span,
+  i {
+    color: $light;
+    transition: color 0.1s ease-in-out;
+  }
+  i {
+    font-size: 1rem;
+  }
+  &__name {
+    text-align: end;
+    margin-left: 0.5rem;
+  }
+}
+</style>
---
+++ frontend/src/components/Select.vue
@@ -0,0 +1,183 @@
+<template>
+  <div class="select" :class="{ active: stateOpenSelect }">
+    <div
+      :class="'select--name' + ' ' + field"
+      :style="{ 'z-index': order }"
+      @click="stateOpenSelect = !stateOpenSelect"
+    >
+      <span class="content">{{ selectvalue ?? name }}</span>
+      <i class="fas fa-chevron-up"></i>
+      <div class="select--name__popup" :class="{ active: stateOpenSelect }">
+        <span @click="getvalueSelect(null)">bỏ chọn</span>
+        <span
+          v-for="(item, index) in dataSelect"
+          :key="index"
+          @click="getvalueSelect(item)"
+          >{{ item }}</span
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, watch } from "vue";
+import anime from "animejs";
+
+export default {
+  props: {
+    name: {
+      type: String,
+    },
+    dataSelect: {
+      type: Object || Array,
+    },
+    field: {
+      type: String,
+    },
+    order: {
+      type: String,
+    },
+  },
+  setup(props) {
+    const stateOpenSelect = ref(false);
+    const selectvalue = ref(undefined);
+    const doneSelect = ref(false);
+    watch(
+      () => stateOpenSelect.value,
+      (oldState) => {
+        if (oldState) {
+          activeSelect();
+        } else {
+          unActiveSelect();
+        }
+      }
+    );
+    function activeSelect() {
+      anime({
+        targets: `.select--name.${props.field} .select--name__popup`,
+        translateY: ["105%", "101%"],
+        opacity: 1,
+      });
+      anime({
+        targets: `.select--name.${props.field} i`,
+        rotate: ["0", "180"],
+        duration: 500,
+      });
+    }
+    function unActiveSelect() {
+      anime({
+        targets: `.select--name.${props.field} .select--name__popup`,
+        translateY: ["101%", "105%"],
+        opacity: 0,
+      });
+      anime({
+        targets: `.select--name.${props.field} i`,
+        rotate: ["180", "0"],
+        duration: 500,
+      });
+    }
+    return {
+      doneSelect,
+      selectvalue,
+      stateOpenSelect,
+    };
+  },
+  methods: {
+    getvalueSelect(value) {
+      if (value === null) {
+        this.selectvalue = undefined;
+      } else if (this.selectvalue !== value) {
+        this.selectvalue = value;
+        this.$emit("changeSelect", { type: this.$props.field, value });
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.select {
+  position: relative;
+  margin: 0.5rem;
+  transition: box-shadow top left 0.2s ease-in-out;
+  &.active {
+    & > div {
+      box-shadow: 0px 0px 0px $main-color;
+      top: 1px;
+      left: 1px;
+    }
+  }
+  &--name {
+    padding: 0.5rem;
+    min-width: 8.125rem;
+    width: fit-content;
+    background-color: $main-color;
+    color: $light;
+    border: 2px solid $bg-select;
+    border-radius: 10px;
+    display: flex;
+    justify-content: space-between;
+    cursor: pointer;
+    position: relative;
+    box-sizing: border-box;
+    box-shadow: 2px 2px 0px $main-color;
+    .content {
+      text-transform: capitalize;
+    }
+    i {
+      margin-left: 0.4rem;
+    }
+    &__popup {
+      &.active {
+        display: block;
+      }
+      position: absolute;
+      bottom: -0.6rem;
+      left: -2px;
+      transform: translateY(100%);
+      background-color: $main-color;
+      border: 2px solid $main-color;
+      width: 100%;
+      border-radius: 10px;
+      border: 2px solid $bg-select;
+      opacity: 0;
+      display: none;
+      // transform: translateY(105%);
+      span {
+        text-transform: capitalize;
+      }
+      &::before {
+        content: "";
+        display: block;
+        position: absolute;
+        top: -1px;
+        left: 20%;
+        transform: translate(-50%, -50%) rotate(-45deg);
+        border: 5px solid $bg-select;
+        border-radius: 2px;
+        border-left-color: transparent;
+        border-bottom-color: transparent;
+      }
+      span {
+        padding: 0.5rem;
+        display: block;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        &:first-child {
+          border-top-left-radius: 8px;
+          border-top-right-radius: 8px;
+        }
+        &:last-child {
+          border-bottom-right-radius: 8px;
+          border-bottom-left-radius: 8px;
+        }
+        &:hover {
+          background-color: $bg-select;
+        }
+      }
+    }
+  }
+}
+</style>
